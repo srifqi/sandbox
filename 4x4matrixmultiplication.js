d133bf5ec8a30aa78114f2314aaf13d43f9e19ed
srifqi <muhammadrifqipriyosusanto@gmail.com>
2014-09-19 20:16:48 +0700

    Update 4x4matrixmultiplication.js

--- 4x4matrixmultiplication.js
+++ 4x4matrixmultiplication.js
@@ -9,13 +9,19 @@ var m2  = [
 0,0,1,0,
 0,0,0,1];
 
-function matrix ( m1 , m2) {
-var M= [];
-for(var i=0;i<16;i+=4){
-for(var u=0;u<4;u++){
-M.push (m1[ i] *m2[ u ] + m1 [ i+1 ] * m2 [ u +4 ] + m1[i+2] * m2 [u+8] + m1 [i+3 ] * m2 [u + 12 ])
-}}
-return M ;
+var matrix = function(a,b){	//a 4x4 matrix in array
+	var c=[];	//result
+	for(var d=0;d<16;d+=4){		//for every first matrix's row
+		for(var e=0;e<4;e++)	//for every second matrix's column
+			//calculate it
+			c.push(
+				a[d]*b[e]+
+				a[d+1]*b[e+4]+
+				a[d+2]*b[e+8]+
+				a[d+3]*b[e+12]
+			);
+	}
+	return c	//return an array
 }
 
 document.body.innerHTML = matrix(m1,m2).join(",");
